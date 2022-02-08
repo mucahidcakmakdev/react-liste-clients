adf8225fa9854e098dff60840458bdd77eb50206
mucahidcakmakdev <mucahidcakmakdev@github.com>
2022-02-08 01:12:48 +0100

    Updates

---
+++ precache-manifest.be21bf4ff013dcc5171f3fabb84cc564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a8ae78209e455ad0cd0",
+    "url": "/react-liste-clients/static/css/main.b0f8a7cb.chunk.css"
+  },
+  {
+    "revision": "7a8ae78209e455ad0cd0",
+    "url": "/react-liste-clients/static/js/main.66b35c0b.chunk.js"
+  },
+  {
+    "revision": "49183a938de3a528d30a",
+    "url": "/react-liste-clients/static/js/runtime~main.3b66e2d5.js"
+  },
+  {
+    "revision": "0e041371cfd6fd314ca1",
+    "url": "/react-liste-clients/static/css/2.17e79147.chunk.css"
+  },
+  {
+    "revision": "0e041371cfd6fd314ca1",
+    "url": "/react-liste-clients/static/js/2.c831b72d.chunk.js"
+  },
+  {
+    "revision": "b2fca1eb463d8c4ed38499a69e6ba653",
+    "url": "/react-liste-clients/index.html"
+  }
+];
